ce94a08756e52ea7451e3d8a533901c92ee0d42e
chenygdlut <cygAriel@mail.dlut.edu.cn>
2020-08-01 14:09:48 +0800

    修改gcd函数

--- 模数求和.py
+++ 模数求和.py
@@ -12,7 +12,7 @@ def gcd(a,b):  # 递归求最大公约数
         return gcd(b,a%b)
 def get_lcm(L): #求一个列表中多个数的最小公倍数
     def lcm(a,b):  #求最小公倍数 ab之积除以最大公约数
-        gcd = lambda a, b: a if b == 0 else gcd(b, a % b)
+        # gcd = lambda a, b: a if b == 0 else gcd(b, a % b)
         return a*b//gcd(a,b) 
     return reduce(lcm,L)  #reduce() 函数会对参数序列中元素进行计算累积，[3,4,5]：先对3,4进行函数计算，再对result和5进行计算。
 line=sys.stdin.readline().strip()
