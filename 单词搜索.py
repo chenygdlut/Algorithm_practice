7445f547fddc1226c4cdd48687c51f1d9161277e
chenygdlut <cygAriel@mail.dlut.edu.cn>
2020-09-20 11:40:43 +0800

    update

--- 单词搜索.py
+++ 单词搜索.py
@@ -1,16 +1,16 @@
 loc=[[0,1],[0,-1],[1,0],[-1,0]]
 def search(board,word,index,x,y,m,n,visited):
-        if index==len(word)-1:
-            return board[x][y]==word[index]
-        if board[x][y]==word[index]:
-            visited[x][y]=1
-            for l in loc:
-                newx,newy=x+l[0],y+l[1]
-                if 0<=newx<m and 0<=newy<n and visited[newx][newy]==0 :
-                    if search(board,word,index+1,newx,newy,m,n,visited):
-                        return True
-            visited[x][y]=0
-        return False
+    if index==len(word)-1:
+        return board[x][y]==word[index]
+    if board[x][y]==word[index]:
+        visited[x][y]=1
+        for l in loc:
+            newx,newy=x+l[0],y+l[1]
+            if 0<=newx<m and 0<=newy<n and visited[newx][newy]==0 :
+                if search(board,word,index+1,newx,newy,m,n,visited):
+                    return True
+        visited[x][y]=0
+    return False
 
 def exist( board, word):
     m=len(board)
